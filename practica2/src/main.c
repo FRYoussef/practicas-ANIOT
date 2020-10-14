550f4ac0d47e9b6512df5fd96257dd6ff06d6994
“FRYoussef” <f.r.youssef@hotmail.com>
2020-10-14 12:44:49 -0400

    Added circular queue

--- practica2/src/main.c
+++ practica2/src/main.c
@@ -4,25 +4,59 @@
 #include "freertos/queue.h"
 #include "esp_log.h"
 
+#define QUEUE_SIZE 5
+
 static float WEIGHTS[] = {0.05, 0.1, 0.15, 0.25, 0.45};
 
+typedef struct CircularQueue {
+   int32_t  counter;
+   int32_t *values;
+};
 
-struct SensorArgs {
+void init_queue(struct CircularQueue *queue){
+    int i;
+
+    queue->values = (int32_t *) malloc(QUEUE_SIZE * sizeof(int32_t));
+    queue->counter = 0;
+
+    for(i = 0; i < QUEUE_SIZE; i++)
+        queue->values[i] = 0;
+}
+
+void free_queue(struct CircularQueue *queue){
+    free(queue->values);
+}
+
+int32_t size_queue(struct CircularQueue *queue){
+    return queue->counter;
+}
+
+int32_t get_element(struct CircularQueue *queue){
+    return queue->values[queue->counter++];
+}
+
+void set_element(struct CircularQueue *queue, int32_t element){
+    queue->values[queue->counter] = element;
+    queue->counter = (queue->counter + 1) % QUEUE_SIZE;
+}
+
+
+typedef struct SensorArgs {
    int32_t  milis;
    QueueHandle_t *queue;
 };
 
-struct FilterArgs {
+typedef struct FilterArgs {
    QueueHandle_t *in_queue;
    QueueHandle_t *out_queue;
 };
 
-struct SensorSample {
+typedef struct SensorSample {
    int32_t sample;
    char *timestamp;
 };
 
-struct FilterSample {
+typedef struct FilterSample {
    float sample;
    char *timestamp;
 };
@@ -31,13 +65,16 @@ struct FilterSample {
 void sensorTask(void *pvparameters){
     struct SensorArgs *args = (struct SensorArgs *) pvparameters;
     struct SensorSample sample;
+    BaseType_t is_write;
 
     while(1){
         // just takes values inside int32_t [INT32_MIN, INT32_MAX]
         sample.sample = (int32_t) esp_random();
         sample.timestamp = esp_log_system_timestamp();
 
-        xQueueSendToFront(args->queue, (void *) &sample, 100);
+        // keep trying if queue is full
+        do { is_write = xQueueSendToFront(args->queue, (void *) &sample, 20); } while(!is_write);
+
         vTaskDelay(args->milis);
     }
     vTaskDelete(NULL);
@@ -46,10 +83,16 @@ void sensorTask(void *pvparameters){
 
 void filterTask(void *pvparameters){
     struct FilterArgs *args = (struct FilterArgs *) pvparameters;
+    struct FilterSample sample;
+    struct CircularQueue queue;
+
+    init_queue(&queue);
 
     while(1){
 
     }
+
+    free_queue(&queue);
     vTaskDelete(NULL);
 }
 
@@ -60,10 +103,10 @@ void app_main() {
     struct FilterArgs f_args1, f_args2;
     QueueHandle_t s_queue1, s_queue2, f_queue1, f_queue2;
 
-    s_queue1 = xQueueCreate(5, sizeof(struct SensorSample));
-    s_queue2 = xQueueCreate(5, sizeof(struct SensorSample));
-    f_queue1 = xQueueCreate(5, sizeof(struct FilterSample));
-    f_queue2 = xQueueCreate(5, sizeof(struct FilterSample));
+    s_queue1 = xQueueCreate(QUEUE_SIZE, sizeof(struct SensorSample));
+    s_queue2 = xQueueCreate(QUEUE_SIZE, sizeof(struct SensorSample));
+    f_queue1 = xQueueCreate(QUEUE_SIZE, sizeof(struct FilterSample));
+    f_queue2 = xQueueCreate(QUEUE_SIZE, sizeof(struct FilterSample));
 
     s_args1.milis = CONFIG_S1_MILLIS;
     s_args1.queue = &s_queue1;
