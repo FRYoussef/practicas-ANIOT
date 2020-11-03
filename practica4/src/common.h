a0650884e95b40e1ff5edc8d014c9c47e37181ed
“FRYoussef” <f.r.youssef@hotmail.com>
2020-11-03 13:39:36 -0500

    Added main configuration

--- practica4/src/common.h
+++ practica4/src/common.h
@@ -6,20 +6,41 @@
 #include "freertos/task.h"
 #include "freertos/timers.h"
 #include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "esp_intr_alloc.h"
+#include "driver/gpio.h"
+#include "driver/touch_pad.h"
 
-static TimerHandle_t chrono;
-static TimerHandle_t resetTimer;
-static TimerHandle_t printTimer;
+#define EV_START 1
+#define EV_STOP 2
+#define EV_RESTART 3
 
-static int32_t seconds;
-static SemaphoreHandle_t seconds_mutex;
+#define ST_INI 1
+#define ST_RUNNING 2
+#define ST_STOPED 3
 
-void timeUpdateTask(void *pvparameters);
-void switchChronoTask(void *pvparameters);
-void timerISR();
-void touchSensorISR();
-void chronometerCallback();
+#define CHRONO_TIMER_ID 1
+#define RESET_TIMER_ID 2
+
+#define ESP_INTR_FLAG_DEFAULT 0
+#define TOUCHPAD_FILTER_TOUCH_PERIOD (10)
+
+#define GPIO_OUTPUT_IO_0 18
+#define GPIO_OUTPUT_PIN_SEL (1ULL<<GPIO_OUTPUT_IO_0)
+
+#define GPIO_INPUT_IO_0 4
+#define GPIO_INPUT_PIN_SEL (1ULL<<GPIO_INPUT_IO_0)
+
+static uint32_t pad_val;
+
+void touchSensorTask(void *pvparameters);
+void timerTask(void *pvparameters);
+void FSMTask(void *pvparameters);
+
+static void IRAM_ATTR timerIsr(void *args);
+static void touchSensorIsr(void *args);
+
+void chronoCallback();
 void resetTimerCallback();
-void printTimerCallback();
 
 #endif
--- practica4/src/main.c
+++ practica4/src/main.c
@@ -3,10 +3,62 @@
 
 void app_main() {
     int32_t prio = uxTaskPriorityGet(NULL);
-    seconds_mutex = xSemaphoreCreateMutex();
 
-    xTaskCreatePinnedToCore(&timeUpdateTask, "timeUpdateTask", 512, NULL, prio, NULL, 0);
-    xTaskCreatePinnedToCore(&switchChronoTask, "switchChronoTask", 512, NULL, prio, NULL, 1);
+    QueueHandle_t event_queue = xQueueCreate(CONFIG_QUEUE_SIZE, sizeof(unsigned char));
+    SemaphoreHandle_t touchSensorSem = xSemaphoreCreateBinary();
+    SemaphoreHandle_t timerSem = xSemaphoreCreateBinary();
+
+    // gpio configuration
+    // output pin
+    gpio_config_t io_conf; 
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en= 0;
+    gpio_config(&io_conf);
+
+    //input pin
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, timerIsr, (void*) timerSem);
+
+    // touch pad configuration
+    touch_pad_init();
+    touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
+
+    for (int i = 0; i< TOUCH_PAD_MAX; i++)
+        touch_pad_config(i, 0);
+
+    touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
+    touch_pad_read_filtered(0, &pad_val);
+    touch_pad_isr_register(touchSensorIsr, &touchSensorSem);
+    touch_pad_intr_enable();
+
+    // timers configuration
+    TimerHandle_t chrono = xTimerCreate("chrono",    // Just a text name, not used by the kernel.
+                         ( CONFIG_CHRONO_TIME * portTICK_PERIOD_MS ),   // The timer period in ticks.
+                         pdTRUE,        // The timers will auto-reload themselves when they expire.
+                         ( void * ) CHRONO_TIMER_ID,  // Assign each timer a unique id equal to its array index.
+                         chronoCallback // Each timer calls the same callback when it expires.
+    );
+
+    TimerHandle_t resetTimer = xTimerCreate("resetTimer",    // Just a text name, not used by the kernel.
+                             ( CONFIG_CHRONO_TIME * portTICK_PERIOD_MS ),   // The timer period in ticks.
+                             pdTRUE,        // The timers will auto-reload themselves when they expire.
+                             ( void * ) RESET_TIMER_ID,  // Assign each timer a unique id equal to its array index.
+                             resetTimerCallback // Each timer calls the same callback when it expires.
+    );
+
+    // tasks configutation
+    xTaskCreatePinnedToCore(&touchSensorTask, "touchSensorTask", 512, &event_queue, prio, NULL, 0);
+    xTaskCreatePinnedToCore(&timerTask, "timerTask", 512, &event_queue, prio, NULL, 0);
+    xTaskCreatePinnedToCore(&FSMTask, "FSMTask", 3094, &event_queue, prio, NULL, 1);
 
     while(1) { vTaskDelay(1000); }
 }
