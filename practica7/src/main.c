8c4f14f4043b1e0d22b29d6b855442d0389a72c4
“FRYoussef” <f.r.youssef@hotmail.com>
2020-12-01 12:20:23 -0500

    Added practica7 skeleton

---
+++ practica7/src/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/adc.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+
+#define N_SAMPLES 5
+static const char* TAG = "Practica7";
+static float sample;
+
+float measure_hall_sensor() {
+    int32_t m = 0;
+
+    for(int i = 0; i < N_SAMPLES; i++)
+        m += hall_sensor_read();
+
+    return (float)(m / N_SAMPLES);
+}
+
+
+void monitor_sensor() {
+    sample = measure_hall_sensor();
+    ESP_LOGI(TAG, "Sample: %f", sample);
+}
+
+
+void app_main() {
+    esp_timer_handle_t timer;
+
+    // mandatory for hall sensor
+    adc1_config_width(ADC_WIDTH_BIT_12);
+
+    const esp_timer_create_args_t timer_args = {
+        .callback = &monitor_sensor,
+        .name = "timer"
+    };
+
+    esp_timer_create(&timer_args, &timer);
+    esp_timer_start_periodic(timer, CONFIG_TIMER_DELAY);
+
+    while(1) { vTaskDelay(pdMS_TO_TICKS(CONFIG_TIMER_DELAY)); }
+
+    esp_timer_delete(timer);
+}
